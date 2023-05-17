8cfec5e2a90e29e3224c97e6bf1ea30fba96e043
Christopher Jaeger <christopher@mogler.dev>
2023-05-17 23:54:01 +0200

    moved Subject to Observer.scala

--- src/main/scala/de/htwg/se_cust_man/Observer.scala
+++ src/main/scala/de/htwg/se_cust_man/Observer.scala
@@ -1,5 +1,19 @@
 package de.htwg.se_cust_man
 
+// ------------------------------------------------------------------------
+// Observer [0/4]
+// https://refactoring.guru/design-patterns/observer
+
 trait Observer {
     def onNotify(): Unit
+}
+
+class Subject {
+    var subscribers: Vector[Observer] = Vector()
+
+    def subscribe(subscriber: Observer): Unit = subscribers = subscribers :+ subscriber
+
+    def unsubscribe(subscriber: Observer): Unit = subscribers = subscribers.filter(a => a != subscriber)
+
+    def notifyObservers() : Unit = subscribers.foreach(a => a.onNotify())
 }
