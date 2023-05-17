15ce16ef492f4461b002ab744184a024c2a75092
Christopher Jaeger <christopher@mogler.dev>
2023-05-17 23:54:17 +0200

    added Controllers

---
+++ src/main/scala/de/htwg/se_cust_man/Controllers.scala
@@ -0,0 +1,21 @@
+package de.htwg.se_cust_man
+
+
+// ------------------------------------------------------------------------
+// Controller 
+
+class CustomerController extends Subject {
+
+}
+
+class UserController extends Subject {
+
+}
+
+class HistoryController extends Subject {
+
+}
+
+class ProjectController extends Subject {
+
+}
