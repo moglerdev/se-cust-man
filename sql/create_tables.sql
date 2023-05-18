238f5df39d57c500ab4ce2b0720c586080bd6b13
Christopher Jaeger <christopher@mogler.dev>
2023-05-19 00:24:11 +0200

    fixed issue in created scripts SQL

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,32 +1,16 @@
 ROLLBACK;
 BEGIN;
+
 create table account
 (
-    id               integer      not null,
-    username         varchar(255) not null,
-    email            varchar(255) not null,
+    id               integer      not null CONSTRAINT user_pk PRIMARY KEY,
+    username         varchar(255) not null CONSTRAINT user_u_username UNIQUE,
+    email            varchar(255) not null CONSTRAINT user_u_email UNIQUE,
     hashed_password varchar(255) not null,
-    name             varchar(255) not null,
-    constraint user_pk
-        primary key (),
-    constraint user_username_unique
-        unique (),
-    constraint user_email_unique
-        unique ()
+    name             varchar(255) not null
 );
 
-create unique index user_pk
-    on account (id);
-
-create unique index user_username_unique
-    on account (username);
-
-create unique index user_email_unique
-    on account (email);
-
-
 ----------------------------------------------------------------------
-
 
 create table address
 (
@@ -39,48 +23,32 @@ create table address
     iso_code varchar(8)   not null
 );
 
-
 ----------------------------------------------------------------------
-
 
 create table customer
 (
-    id         integer      not null,
+    id         integer      not null CONSTRAINT customer_pk PRIMARY KEY,
     name       varchar(255) not null,
-    address_id integer      not null,
+    address_id integer      not null CONSTRAINT customer_fk_address REFERENCES address(id),
     email      varchar(255) not null,
-    phone      varchar(255) not null,
-    constraint customer_pk
-        primary key ()
+    phone      varchar(255) not null
 );
-
-create unique index customer_pk
-    on customer (id);
-
 
 ----------------------------------------------------------------------
 
-
 create table history
 (
-    id         integer                 not null,
+    id         integer                 not null CONSTRAINT history_pk PRIMARY KEY,
     objectname varchar(255)            not null,
     objectid   integer                 not null,
     action     char
         constraint history_action_enum
-            check (action = ANY (ARRAY ['N'::bpchar, 'S'::bpchar, 'D'::bpchar])),
-    account_id integer                 not null,
-    time_stamp timestamp default now() not null,
-    constraint history_pk
-        primary key ()
+            check (action IN ('N', 'S', 'D')),
+    account_id integer                 not null CONSTRAINT history_fk_account REFERENCES account(id),
+    time_stamp timestamp default now() not null
 );
 
-create unique index history_pk
-    on history (id);
-
-
 ----------------------------------------------------------------------
-
 
 create table history_change
 (
@@ -89,48 +57,35 @@ create table history_change
             primary key,
     history_id integer      not null
         constraint history_change_fk_history
-            references ??? (),
+            references history(id),
     field_name varchar(255) not null,
     old_value  text         not null
 );
 
-
 ----------------------------------------------------------------------
-
 
 create table project
 (
-    id          integer                                     not null,
+    id          integer                                     not null CONSTRAINT project_pk PRIMARY KEY,
     title       varchar(255)                                not null,
-    description varchar(1024) default ''::character varying not null,
+    description varchar(1024) default '' not null,
     deadline    date          default now()                 not null,
-    customer_id integer,
-    constraint project_pk
-        primary key ()
+    customer_id integer CONSTRAINT project_fk_customer REFERENCES customer(id)
 );
-
-create unique index project_pk
-    on project (id);
-
 
 ----------------------------------------------------------------------
 
-
 create table task
 (
-    id             integer                                     not null,
+    id             integer                                     not null CONSTRAINT task_pk PRIMARY KEY,
     title          varchar(255)                                not null,
-    description    varchar(1024) default ''::character varying not null,
+    description    varchar(1024) default ''  not null,
     project_id     integer
         constraint task_fk_project
             references project (id),
     estimated_time double precision                            not null,
     required_time  double precision
 );
-
-create unique index task_pk
-    on task (id);
-
 
 COMMIT;
 END;
