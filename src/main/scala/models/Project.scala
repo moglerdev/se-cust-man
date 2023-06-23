6e0b04e183a4fbb5b63a6c847aa3c5886afae7f5
Dennis Schulze <dennis.s1.1@gmx.net>
2023-06-23 09:44:56 +0200

    added Project

---
+++ src/main/scala/models/Project.scala
@@ -0,0 +1,12 @@
+package de.htwg.scm
+package models
+
+import java.time.LocalDateTime
+
+case class Project(id: Int, customer_id: Int, title: String, description: String)
+
+object Project {
+  def empty(customer: Customer): Project = {
+    Project(-1, customer.id, "", "")
+  }
+}
