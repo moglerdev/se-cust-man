47bed98cf3fa16e28254c586cf44f32ae8b9cb8a
Dennis Schulze <dennis.s1.1@gmx.net>
2023-06-24 16:25:19 +0200

    Kommentare für Controller

--- src/main/scala/controller/CustomerController.scala
+++ src/main/scala/controller/CustomerController.scala
@@ -6,12 +6,15 @@ import com.google.inject.{Guice, Injector}
 import de.htwg.scm.store._
 import net.codingwell.scalaguice.InjectorExtensions.*
 
+// Controller for managing customers
 class CustomerController(store: ICustomerStore) extends ModelController[Customer](store) with ICustomerController {
 
+  // Retrieves the customer associated with the specified project
   def getByProject(project: Project): Option[Customer] = {
     store.getAll.find(_.id == project.customer_id)
   }
 
+  // Filters the customers based on the specified criteria
   override def filter(name: Option[String], email: Option[String], phone: Option[String]): List[Customer] = {
     store.getAll.filter(c => {
       val nameFilter = name match {
--- src/main/scala/controller/TaskController.scala
+++ src/main/scala/controller/TaskController.scala
@@ -7,11 +7,15 @@ import net.codingwell.scalaguice.InjectorExtensions.*
 import com.google.inject.Guice
 import store._
 
+// Controller for managing tasks
 class TaskController(store: ITaskStore) extends ModelController[Task](store) with ITaskController {
+
+  // Retrieves all tasks associated with a project
   override def getAllByProject(project: Project): List[Task] = {
     store.getAll.filter(task => task.project_id == project.id)
   }
 
+  // Filters tasks based on title and description criteria
   override def filter(title: Option[String], description: Option[String]): List[Task] = {
     store.getAll.filter(task => {
       title match {
