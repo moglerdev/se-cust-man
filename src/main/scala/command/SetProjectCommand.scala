08edc9fb8d40d900f20ac28b7d0b282858a3f4b9
Christopher Jaeger <christopher@mogler.dev>
2023-06-24 17:22:15 +0200

    added "Open Commands" and "Set Commands"

---
+++ src/main/scala/command/SetProjectCommand.scala
@@ -0,0 +1,30 @@
+package de.htwg.scm
+package command
+
+import model.Project
+import state.ProjectState
+
+case class SetProjectCommand(state: ProjectState, customer_id: Int, title: Option[String], description: Option[String]) extends ICommand {
+  val project: Option[Project] = state.get
+
+  override def execute(): Boolean = {
+    val c = project.getOrElse(Project(-1, -1, "", ""))
+    val toSet = Project(
+      c.id,
+      customer_id,
+      title.getOrElse(c.title),
+      description.getOrElse(c.description),
+    )
+    state.set(Some(toSet))
+    true
+  }
+
+  override def undo(): Boolean = {
+    state.set(project)
+    true
+  }
+
+  override def redo(): Boolean = {
+    execute()
+  }
+}
