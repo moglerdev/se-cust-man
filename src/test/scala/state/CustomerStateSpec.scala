9542805fde7cfbec9cfb1df0fbd18ea72e3887e7
Christopher Jaeger <christopher@mogler.dev>
2023-06-24 23:23:57 +0200

    fixed Store Tests

--- src/test/scala/state/CustomerStateSpec.scala
+++ src/test/scala/state/CustomerStateSpec.scala
@@ -1,34 +1,81 @@
+package de.htwg.scm
 package state
 
-import org.scalatest.flatspec.AnyFlatSpec
+import model.Customer
 import org.scalatest.matchers.should.Matchers
-import de.htwg.scm.model.Customer
-import de.htwg.scm.state.CustomerState
+import org.scalatest.wordspec.WordSpec
 
-class CustomerStateSpec extends AnyFlatSpec with Matchers {
+class CustomerStateSpec extends WordSpec with Matchers {
 
-  "A CustomerState" should "initially have no customer" in {
-    val customerState = new CustomerState()
+  "CustomerState" should {
 
-    customerState.get should be(None)
-  }
+    "set and retrieve the customer" in {
+      // Create a customer
+      val customer = Customer(1, "John Doe", "john@example.com", "1234567890", "123 Main St")
 
-  it should "set and retrieve a customer" in {
-    val customerState = new CustomerState()
-    val customer = Customer(1, "John Doe", "john@example.com", "123456789", "123 Main St")
+      // Create a customer state
+      val state = new CustomerState()
 
-    customerState.set(Some(customer))
+      // Set the customer
+      state.set(Some(customer))
 
-    customerState.get should be(Some(customer))
-  }
+      // Retrieve the customer
+      val retrievedCustomer = state.get
 
-  it should "allow setting the customer to None" in {
-    val customerState = new CustomerState()
-    val customer = Customer(1, "John Doe", "john@example.com", "123456789", "123 Main St")
+      // Verify that the retrieved customer matches the original customer
+      retrievedCustomer should be(customer)
+    }
 
-    customerState.set(Some(customer))
-    customerState.set(None)
+    "return the customer option" in {
+      // Create a customer
+      val customer = Customer(1, "John Doe", "john@example.com", "1234567890", "123 Main St")
 
-    customerState.get should be(None)
+      // Create a customer state
+      val state = new CustomerState()
+
+      // Set the customer
+      state.set(Some(customer))
+
+      // Retrieve the customer option
+      val customerOption = state.option
+
+      // Verify that the customer option is Some(customer)
+      customerOption should be(Some(customer))
+    }
+
+    "return if the customer state is defined" in {
+      // Create a customer state without a customer
+      val state1 = new CustomerState()
+
+      // Verify that the state is not defined
+      state1.isDefined should be(false)
+
+      // Create a customer
+      val customer = Customer(1, "John Doe", "john@example.com", "1234567890", "123 Main St")
+
+      // Create a customer state with a customer
+      val state2 = new CustomerState().set(Some(customer))
+
+      // Verify that the state is defined
+      state2.isDefined should be(true)
+    }
+
+    "return if the customer state is empty" in {
+      // Create a customer state without a customer
+      val state1 = new CustomerState()
+
+      // Verify that the state is empty
+      state1.isEmpty should be(true)
+
+      // Create a customer
+      val customer = Customer(1, "John Doe", "john@example.com", "1234567890", "123 Main St")
+
+      // Create a customer state with a customer
+      val state2 = new CustomerState().set(Some(customer))
+
+      // Verify that the state is not empty
+      state2.isEmpty should be(false)
+    }
+
   }
 }
